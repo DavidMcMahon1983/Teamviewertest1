cc86d77790844f9018f15f4e3dd1a05833a7e7ea
David <David@LAPTOP-DT3LGN29>
2017-02-06 17:10:06 +0000

    First commit of addition program

---
+++ src/teamviewertest1/TeamviewerTest1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamviewertest1;
+
+/**
+ *
+ * @author David
+ */
+public class TeamviewerTest1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a = 12;
+        int b = 4;
+        
+        int add = a + b;
+        int sub = a - b;
+        
+        System.out.println("Adding:::: " + add);
+        System.out.println("Subtracting:::" + sub);
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
